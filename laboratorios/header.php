e642e4695820a2dc6ce42b86d30eedaae3e74af3
nathan03-11 <nathanghuisoli@gmail.com>
2025-09-24 21:45:29 -0300

    Add files via upload

---
+++ laboratorios/header.php
@@ -0,0 +1,33 @@
+<header>
+<h1></h1>
+<nav>
+
+
+
+</nav>
+</header>
+
+<style>
+    
+header {
+  background: #262626;
+  color: #fff;
+  padding: 15px 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+header h1 {
+  margin: 0; 
+  font-size: 1.5em;
+}
+nav a {
+  color: #fff;
+  margin-left: 20px;
+  text-decoration: none;
+  font-weight: bold;
+}
+nav a:hover {
+  text-decoration: underline;
+}
+</style>
